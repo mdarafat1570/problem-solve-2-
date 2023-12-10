e01493640fef88038a268649fa522d6fbe9e804c
Muhammad Arafat <77748809+mdarafat1570@users.noreply.github.com>
2023-12-10 22:57:28 +0600

    loop solveing on java

---
+++ forlooping/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // for (int i=1; i<=100;i++){
+        //   System.out.println("hi");   // for loop
+
+
+// int i =0;
+// while (i<=3){
+//     System.out.println(i+" .android developer office man cyberpunk 77");
+//     i=i+2;
+//             }    // while loop
+//
+//        double i=1;
+//        do {
+//            System.out.println(i+"bangladesh");
+//            i++;
+//
+//        }while (i<=1828292891);  //do while loop
+//     }
+//    }
+
+
+//        Scanner input = new Scanner(System.in);
+//
+//        int  num1 , num2 , velu ;
+//        System.out.println("apani 2 ta numbar lekhun : ");
+//
+//       num1= input.nextInt();
+//       num2=input.nextInt();
+//        velu = (num1>num2)? num1 : num2 ;
+//        System.out.println("apnar numbar hoilo : "+velu); // condition Operator
+//
+//        Scanner input =new Scanner(System.in);
+//        int num;
+//        System.out.println("apanar numbar te den plz :");
+//        num =input.nextInt();
+//
+//        if (num%2==0){
+//            System.out.println("apnar Numbar taa holo (Even)");
+//        }else {
+//            System.out.println("apnar numbar ta holo (Odd)");
+//        }     }
+     Scanner input =new Scanner(System.in);
+        System.out.println("apanar numbar ti den plz :");
+     int num;
+    num= input.nextInt();
+
+        if (num%2==0){
+            System.out.println("apanar numbar ti holo (even) ");
+        }else{
+            System.out.println("apnar numbar ti holo (odd)");
+        }
+   }
+}
