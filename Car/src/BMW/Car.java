85e762c2dc1260b56ad03c0f0c784b9adf21227c
Muhammad Arafat <77748809+mdarafat1570@users.noreply.github.com>
2023-12-10 22:56:12 +0600

    car Code on java

---
+++ Car/src/BMW/Car.java
@@ -0,0 +1,18 @@
+package BMW;
+
+public class Car {
+
+    public static void main(String[] args) {
+
+
+        boolean a;
+        float b;
+        int c;
+        short d;
+        char e;
+        double f;
+
+
+        
+    }
+}
---
+++ Car/src/BMW/login.java
@@ -0,0 +1,5 @@
+package BMW;
+
+public class login {
+    
+}
